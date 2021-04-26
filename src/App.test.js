75c7683d2b8dddd99e9db1965e8b5971bae7e40f
sdelane2 <seanmartindelaney@gmail.com>
2021-04-25 20:57:17 -0400

    Test and code button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct inital color', () => {
   render(<App />);
   const colorButton = screen.getByRole('button', { name: "Change to blue"});
   expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'});
+  expect(colorButton.textContent).toBe('Change to red');
 });
 
-test('button has correct initial text', () => {
+// test('button turns blue when clicked', () => {
+//   render(<App />);
+//   const colorButton = screen.getByRole('button', { name: 'Change to blue'});
 
-});
-
-test('button turns blue when clicked', () => {
-
-});
+// });
