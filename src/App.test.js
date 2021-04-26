bb12d4c935d6c7e3e489d043d3188a28eac9a6f2
sdelane2 <seanmartindelaney@gmail.com>
2021-04-25 20:11:14 -0400

    button color/text test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct inital color', () => {
   render(<App />);
-  const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole('button', { name: "Change to blue"});
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+});
+
+test('button has correct initial text', () => {
+
+});
+
+test('button turns blue when clicked', () => {
+
 });
